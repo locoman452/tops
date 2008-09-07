d5e8b5e41c2711e75aba6ce3510055177a6b8ec5
dkirkby <dkirkby@uci.edu>
2008-09-07 00:48:48 +0000

    add core package

---
+++ core/__init__.py
@@ -0,0 +1,16 @@
+"""
+Core Telescope Operations Software
+
+Core support for the telescope operations software package. All
+project-specific code lives in other packages.
+"""
+
+## @package tops
+# Core Telesope Operations Software
+#
+# @author David Kirkby, dkirkby@uci.edu
+# @version $Revision$
+# @date Created 6-Sep-2008
+# @date Last changed $LastChangedDate$ by $LastChangedBy$
+#
+# $URL$
