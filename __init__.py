fe578efa22945341f882745749bbfec4bfd50f17
dkirkby <dkirkby@uci.edu>
2008-09-07 00:27:02 +0000

    Create top-level dir

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+Telesope Operations Software
+
+A python framework for managing the real-time operations of a
+distributed and heterogeneous platform of control and monitoring
+devices, such as a telescope and its associated instruments.
+"""
+
+## @package tops
+# Telesope Operations Software
+#
+# @author David Kirkby, dkirkby@uci.edu
+# @version $Revision$
+# @date Created 1-Sep-2008
+# @date Last changed $LastChangedDate$ by $LastChangedBy$
+#
+# $URL$
