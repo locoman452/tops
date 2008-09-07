67e8d337b4dbde88c7db57b0fb75cac3a28a8b6e
dkirkby@uci.edu <dkirkby@uci.edu@4c0c3218-2856-0410-8d27-1537a1cf72ec>
2008-09-07 01:09:45 +0000

    add time module

--- trunk/core/__init__.py
+++ trunk/core/__init__.py
@@ -9,8 +9,6 @@ project-specific code lives in other packages.
 # Core Telesope Operations Software
 #
 # @author David Kirkby, dkirkby@uci.edu
-# @version $Revision$
 # @date Created 6-Sep-2008
-# @date Last changed $LastChangedDate$ by $LastChangedBy$
 #
-# $URL$
+# This project is hosted at http://tops.googlecode.com/
