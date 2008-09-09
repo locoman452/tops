2d1109bfeac42f31c33102bb714c84dba4aa1675
dkirkby@uci.edu <dkirkby@uci.edu@4c0c3218-2856-0410-8d27-1537a1cf72ec>
2008-09-09 15:12:03 +0000

    Initial release for APO testing without any network code

---
+++ tags/V0.1/sdss3/design/__init__.py
@@ -0,0 +1,14 @@
+"""
+SDSS3 design documents for the Telescope Operations Software
+
+SDSS-3 project-specific code for the telescope operations software
+package.
+"""
+
+## @package tops.sdss3.design
+# SDSS3 design documents for the Telescope Operations Software
+#
+# @author David Kirkby, dkirkby@uci.edu
+# @date Created 6-Sep-2008
+#
+# This project is hosted at http://tops.googlecode.com/
